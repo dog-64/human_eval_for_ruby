db4c66d270fb2d89fd3226e79ba490fcb3f35d88
dog <olegduletsky@gmail.com>
2025-03-15 18:22:50 +0200

    fix - t116-assert.rb -

--- tasks/t116-assert.rb
+++ tasks/t116-assert.rb
@@ -1,5 +1,5 @@
-assert sort_array([1, 5, 2, 3, 4]) == [1, 2, 3, 4, 5]
+assert sort_array([1, 5, 2, 3, 4]) == [1, 2, 4, 3, 5]
 assert sort_array([-2, -3, -4, -5, -6]) == [-6, -5, -4, -3, -2]
-assert sort_array([1, 0, 2, 3, 4]) == [0, 1, 2, 3, 4]
+assert sort_array([1, 0, 2, 3, 4]) == [0, 1, 2, 4, 3]
 assert sort_array([]) == []
 
